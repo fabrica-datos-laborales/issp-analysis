44966ee236e9fa67652816c2e092848373561a88
valentinaandrade <valentinaandrade@uchile.cl>
2021-07-14 22:16:03 -0400

    class construction and final data to models
    - class edit
    - scales edit

--- R/01proc-descriptivecode.R
+++ R/01proc-descriptivecode.R
@@ -29,7 +29,7 @@ issp <- issp %>%
          v44 = car::recode(.$v44, c("1=0;2=1;3=2;4=3;5=4;6=5;7=6;c(0,8,9)=NA"))) %>% 
   mutate(v42 = labelled(.$v42,c("Very good"=0,"Quite good"=1,"Neither good nor bad"=2,"Quite bad"=3,"Very bad"=4)),
          v44 = labelled(.$v44,c("Completely satisfied"=0,"Very satisfied"=1,"Fairly satisfied"=2,"Neither satisfied nor dissatisfied"=3,"Fairly dissatisfied"=4, "Very dissatisfied"= 5, "Completely dissatisfied" = 6)), 
-        UNION = car::recode(.$UNION, c("c(7,9)=NA")),
+         UNION = car::recode(.$UNION, c("c(7,9)=NA")),
          SEX = car::recode(.$SEX, c("9=NA")),
          DEGREE = car::recode(.$DEGREE, c("9=NA")),
          TYPORG2 = car::recode(.$TYPORG2, c("c(8,9)=NA")),
@@ -37,30 +37,61 @@ issp <- issp %>%
          conflict = case_when(v42 %in% c(4,3) ~ 1,
                               v42 %in% c(2,1,0) ~ 0), 
          year = 2015) %>% 
-# 5. Select  --------------------------------------------------------------
+  # 5. Select  --------------------------------------------------------------
 dplyr::select(country, year, SEX, AGE, DEGREE, TYPORG2, UNION, NEMPLOY, EMPREL, WRKSUP, ISCO08, WORK,
-       v42,v44,v45,v46,v47, conflict, c_alphan,
-       WEIGHT) %>% filter(AGE != 0)
+              v42,v44,v45,v46,v47, conflict, c_alphan,
+              WEIGHT) %>% filter(AGE != 0)
 
 # B. Construct class variable --------------------------------------------------
 # 3. Recode variables ----------------------------------------------------------
 # 3.1 Employment relation ------------------------------------------------------
 
-# A.1 Filter inactive people
-# (1) WORK: Are you currently working for pay, did you work for pay in the past, or have you never been in paid work?
-issp <- filter(issp, WORK != 3) # 8 y 9 NS/NR
+# 3.1 Employment relation ------------------------------------------------------
 
-# A.2 Create salaried workers
-# (2) EMPREL: Are/ were you an employee, self-employed, or working for your own family's business?
-issp$prop_salaried <- as.numeric(issp$EMPREL) 
-issp$prop_salaried <- car::recode(issp$prop_salaried,recodes = "1='Salaried';c(5,2)='3.Petite bourgeoisie';3='2.Small employers';4='1.Capitalist';c(8,9)=NA", as.factor = T,
-                                  levels = c("Salaried", "3.Petite bourgeoisie", "2.Small employers","1.Capitalist"))
+# A.1 Filtrar inactivos
+#Variable WRKTYPE: R: Working for private or public sector or self employed 
+issp <- filter(issp, WORK != 3)
+# --> 3 Never had paid work
+
+# A.2 Crear variable asalariados
+issp$EMPREL <- as.numeric(issp$EMPREL) 
+issp$prop_salariedA <- car::recode(issp$EMPREL,recodes = "c(1,4)='Salaried';2='3.Petite bourgeoisie';3='Employer';c(8,9)=NA")
+
+# Verificar
+issp %>% count(prop_salariedA) %>% mutate(prop = prop.table(n))
+issp %>% count(EMPREL) %>% mutate(prop = prop.table(n))
+
+
+# 4.2 Propietarios -------------------------------------------------------------
+# Proxy: NEMPLOY (How many employees do/ did you have, not counting yourself?)
+# A.1 Crear variable "owners":
+#1.Pequeña burguesia: 0 a 1 empleados
+#2. Pequeños empleadores: de 2 a 9 empleados
+#3. Capitalist: de 10 a más empleados
+issp$owners <- as.numeric(issp$NEMPLOY)
+issp <- issp %>% mutate(owners = case_when(owners %in% c(10:9995)& prop_salariedA=="Employer"~ "1.Capitalist",
+                                           owners %in% c(2:9, 9999,9998)& prop_salariedA=="Employer" ~ "2.Small employers",                                           owners %in% c(2:9)& prop_salariedA=="Employer" ~ "2.Small employers",
+                                           owners %in% c(1)| prop_salariedA=="3.Petite bourgeoisie"~ "3.Petite bourgeoisie",
+                                           TRUE ~ NA_character_))
+
+# Importante --> hay 1 que indican no tener ni 1 a 9995, indican NA asi que son incorporados en petite bourgoise
+# A.2 Verificar
+issp %>% count(owners) %>% mutate(prop = prop.table(n))
+issp %>% count(NEMPLOY) %>% mutate(prop = prop.table(n))
+
+# A.3 Var final salaried -------------------------------------------------------
+issp <- issp %>% mutate(prop_salaried = if_else(is.na(owners), prop_salariedA, owners))
+
+# A.2 Verificar
+issp %>% count(prop_salaried) %>% mutate(prop = prop.table(n))
+issp %>% count(prop_salariedA) %>% mutate(prop = prop.table(n))
+
 
 # 3.2 Salaried workers ---------------------------------------------------------
 # A.1 Supervisan ---------------------------------------------------------------
 # WRKSUP #Do/ did you supervise other employees? Yes/No
 issp$control <- as.numeric(issp$WRKSUP)
-issp$control <- car::recode(issp$control,recodes = "1='Control';2='No control';99=NA",as.factor =T, 
+issp$control <- car::recode(issp$control,recodes = "1='Control';2='No control';c(8,9)=NA",as.factor =T, 
                             levels = c("Control", "No control"))
 
 # A.2 Skills--------------------------------------------------------------------
@@ -128,7 +159,7 @@ issp %>%
 # C.1 Univariate --------------------------------------------------------------
 ## ----Descriptive sociodemographics --------------------------------------
 issp %>% select(SEX, UNION, DEGREE, TYPORG2) %$%
-sjmisc::frq(.)
+  sjmisc::frq(.)
 
 ## ---- Descriptive age------------------------------------------------
 plot_frq(issp$AGE, type = "h", show.mean = TRUE, show.mean.val = TRUE,normal.curve = TRUE, show.sd = TRUE, normal.curve.color = "blue")
@@ -140,22 +171,22 @@ sjPlot::plot_frq(issp$class, weight.by = issp$WEIGHT) + labs(x = "Class position
 
 ## ----Descriptive variables to construct class------------------------------------------
 issp %>% select(prop_salaried, control, educ, qual, skillsA, skills) %$%
-sjmisc::frq(.)
+  sjmisc::frq(.)
 
 ## ----Descriptive dependent variable conflict-------------------------------------------
 sjPlot::plot_frq(issp$conflict, weight.by = issp$WEIGHT, show.ci = T, errorbar.color = "black") + labs(x = "Relations: between management and employees", caption = "Source: E.O.Wright (1997) based on ISSP (2015)")
 
 ## ---- Descriptive v42(non-recode dependent variable ------------------------------------------------
 issp %>% select(v42) %$%
-sjmisc::frq(.)
+  sjmisc::frq(.)
 
 
 # C.2 Bivariate -----------------------------------------------------------
 
 # Sex-conflict ------------------------------------------------------------
 issp %$% 
-tab_xtab(.$SEX, .$conflict,
-         statistics = "cramer", weight.by = .$WEIGHT, show.row.prc = T)
+  tab_xtab(.$SEX, .$conflict,
+           statistics = "cramer", weight.by = .$WEIGHT, show.row.prc = T)
 
 # Union-conflict ------------------------------------------------------------
 issp %$% 
@@ -247,13 +278,13 @@ issp %>%
 # Correlation matrix ------------------------------------------------------
 ## v42,v44, v45, v46 y v47
 issp %>% select(starts_with("v4")) %>% 
-sjp.corr(.,show.legend = TRUE, title = "Correlation plot")
+  sjp.corr(.,show.legend = TRUE, title = "Correlation plot")
 
 
 # Test para FA -----------------------------------------------------------------
 # KMO --------------------------------------------------------------------------
 issp %>% select(starts_with("v4")) %>% 
-psych::KMO(.)
+  psych::KMO(.)
 
 # Test de Bartlett -------------------------------------------------------------
 issp %>% select(starts_with("v4")) %>% 
@@ -261,16 +292,16 @@ issp %>% select(starts_with("v4")) %>%
 
 # Scree plot -------------------------------------------------------------------
 issp %>% select(starts_with("v4")) %>% 
-scree(.)
+  scree(.)
 
 # PCA and Cronbach ---------------------------------------------------------------------
 issp %>%
   select(starts_with("v4")) %>% 
-tab_pca(., rotation =c("varimax"), 
-        title = "Principal Component Analysis with `v4` items", 
-        string.pov = "Proportion of Variance",
-        string.cpov = "Cumulative Proportion", nmbr.fctr = 2,
-        show.msa = T)
+  tab_pca(., rotation =c("varimax"), 
+          title = "Principal Component Analysis with `v4` items", 
+          string.pov = "Proportion of Variance",
+          string.cpov = "Cumulative Proportion", nmbr.fctr = 2,
+          show.msa = T)
 # By Kaiser criteria is only one principal component
 # We force two componentes with nmbr.fctr = 2
 # cronbach is shown bottom
@@ -320,13 +351,13 @@ issp %>%
 issp %>%
   select(starts_with("v4")) %>% 
   corr.test(., alpha = 0.05,
-          method='pearson')
+            method='pearson')
 
 #Alpha cronbach 
 issp %>%
   select(starts_with("v4")) %>% 
   psych::alpha(.)
- 
+
 
 #With different combinations of items (to check)
 issp %>%
@@ -338,169 +369,81 @@ issp %>%
 # scale <- issp %>% select(v42,v45,v46,v47)
 #   homals(scale, ndim = 1, level = "ordinal", active = TRUE)
 
-# Mean index --------------------------------------------------------------
-# index_1 con todas las v4* e index_2 solo con indicadas
+# Mean scale --------------------------------------------------------------
+# scale_1 con todas las v4* e scale_2 solo con indicadas
 
-# 1. Mean index_1 and index_2 by class ----------------------------------------------------------------
+# 1. Mean scale_1 and scale_2 by class ----------------------------------------------------------------
 
 issp %>% 
   select(starts_with("v4"), class, c_alphan, WEIGHT) %>%
-  filter_at(vars(v42:v47), all_vars(!is.na(.))) %>% 
-  mutate_at(vars(starts_with("v4")), ~as.numeric(.)+1) %>%
-  mutate_at(vars(starts_with("v4")), ~.*100/7) %>%
+  filter_at(vars(v42:v47), all_vars(!is.na(.))) %>%
+  mutate_at(vars(starts_with("v4")), ~.*100/6) %>%
   rowwise() %>% 
-  mutate(index_1 = mean(v42:v47, na.rm =  T),
-         index_2 = mean(c(v42,v45,v46,v47),  na.rm =  T)) %>%
+  mutate(scale_1 = mean(v42:v47, na.rm =  T),
+         scale_2 = mean(c(v42,v45,v46,v47),  na.rm =  T)) %>%
   ungroup() %>%
   group_by(class) %>%
-  summarise_at(vars(starts_with("index")), funs(weighted.mean(.,WEIGHT,na.rm = T)))
+  summarise_at(vars(starts_with("scale")), funs(weighted.mean(.,WEIGHT,na.rm = T)))
 
-                
-
-
-# 2. Mean index_1 and index_2 by country -------------------------------------
+# 2. Mean scale_1 and scale_2 by country -------------------------------------
 issp %>% 
   select(starts_with("v4"), class, c_alphan, WEIGHT) %>%
   filter_at(vars(v42:v47), all_vars(!is.na(.))) %>% 
-  mutate_at(vars(starts_with("v4")), ~as.numeric(.)+1) %>%
-  mutate_at(vars(starts_with("v4")), ~.*100/7) %>%
+  mutate_at(vars(starts_with("v4")), ~.*100/6) %>%
   rowwise() %>% 
-  mutate(index_1 = mean(v42:v47, na.rm =  T),
-         index_2 = mean(c(v42,v45,v46,v47),  na.rm =  T)) %>%
+  mutate(scale_1 = mean(v42:v47, na.rm =  T),
+         scale_2 = mean(c(v42,v45,v46,v47),  na.rm =  T)) %>%
   ungroup() %>%
   group_by(c_alphan) %>%
-  summarise_at(vars(starts_with("index")), funs(weighted.mean(.,WEIGHT,na.rm = T))) %>%
+  summarise_at(vars(starts_with("scale")), funs(weighted.mean(.,WEIGHT,na.rm = T))) %>%
   print(n = 46)
 
 # Anova -------------------------------------------------------------------
 issp %>% 
   select(starts_with("v4"), class, c_alphan, WEIGHT) %>%
   filter_at(vars(v42:v47), all_vars(!is.na(.))) %>% 
-  mutate_at(vars(starts_with("v4")), ~as.numeric(.)+1) %>%
-  mutate_at(vars(starts_with("v4")), ~.*100/7) %>%
+  mutate_at(vars(starts_with("v4")), ~.*100/6) %>%
   rowwise() %>% 
-  mutate(index_1 = mean(v42:v47, na.rm =  T),
-         index_2 = mean(c(v42,v45,v46,v47),  na.rm =  T)) %>%
+  mutate(scale_1 = mean(v42:v47, na.rm =  T),
+         scale_2 = mean(c(v42,v45,v46,v47),  na.rm =  T)) %>%
   ungroup() %>%
-  pivot_longer(., cols = c(index_1, index_2), names_to = "type_index") %$% 
-sjPlot::sjp.aov1(.$value, .$type_index, title = "Anova by type index")
+  pivot_longer(., cols = c(scale_1, scale_2), names_to = "type_scale") %$% 
+  sjPlot::sjp.aov1(.$value, .$type_scale, title = "Anova by type scale")
 
 
-# Anova index by class ----------------------------------------------------
+# Anova scale by class ----------------------------------------------------
 issp %>% 
   select(starts_with("v4"), class, c_alphan, WEIGHT) %>%
   filter_at(vars(v42:v47), all_vars(!is.na(.))) %>% 
-  mutate_at(vars(starts_with("v4")), ~as.numeric(.)+1) %>%
-  mutate_at(vars(starts_with("v4")), ~.*100/7) %>%
+  mutate_at(vars(starts_with("v4")), ~.*100/6) %>%
   rowwise() %>% 
-  mutate(index_1 = mean(v42:v47, na.rm =  T),
-         index_2 = mean(c(v42,v45,v46,v47),  na.rm =  T)) %>%
+  mutate(scale_1 = mean(v42:v47, na.rm =  T),
+         scale_2 = mean(c(v42,v45,v46,v47),  na.rm =  T)) %>%
   ungroup()  %$% 
-  sjPlot::sjp.aov1(.$index_2, .$class, title = "Anova index N°2 by class")
+  sjPlot::sjp.aov1(.$scale_2, .$class, title = "Anova scale N°2 by class")
 
-# Anova index by class ----------------------------------------------------
+# Anova scale by country ----------------------------------------------------
 issp %>% 
   select(starts_with("v4"), class, c_alphan, WEIGHT) %>%
   filter_at(vars(v42:v47), all_vars(!is.na(.))) %>% 
-  mutate_at(vars(starts_with("v4")), ~as.numeric(.)+1) %>%
-  mutate_at(vars(starts_with("v4")), ~.*100/7) %>%
+  mutate_at(vars(starts_with("v4")), ~.*100/6) %>%
   rowwise() %>% 
-  mutate(index_1 = mean(v42:v47, na.rm =  T),
-         index_2 = mean(c(v42,v45,v46,v47),  na.rm =  T)) %>%
+  mutate(scale_1 = mean(v42:v47, na.rm =  T),
+         scale_2 = mean(c(v42,v45,v46,v47),  na.rm =  T)) %>%
   ungroup()  %$% 
-  sjPlot::sjp.aov1(.$index_2, .$c_alphan, title = "Anova index N°2 by country")
+  sjPlot::sjp.aov1(.$scale_2, .$c_alphan, title = "Anova scale N°2 by country")
 
-
-# New scale 1 (prueba) ----------------------------------------------------
-## v44: from 0 - 6 to 0 - 4
-summary(issp$v44)
-
-issp$v44_0_4 <- issp$v44*0.66666666667
-frq(issp$v44_0_4)
-summary(issp$v44_0_4)
-mean(issp$v44_0_4, na.rm = T)
-sd(issp$v44_0_4, na.rm = T)
-
-## Final scale1
-issp$scale1 <- ((issp$v42 + issp$v44_0_4 + issp$v45 + issp$v46 + issp$v47)/20)*100
-frq(issp$scale1)
-summary(issp$scale1)
-mean(issp$scale1, na.rm = T)
-sd(issp$scale1, na.rm = T)
-
-
-## Scale 1 by class (example)-
-issp %>% 
-  group_by(class) %>%
-  filter(!is.na(class)) %>% 
-  summarise(n(),mean=mean(scale1,na.rm = TRUE), sd=sd(scale1,na.rm = TRUE))
-
-##ANOVA Scale1 by class (EXAMPLE)---
-anova_class_scale1 <- aov(scale1~class, data = issp)
-summary(anova_class_scale1)
-
-
-## Scale 1 by country (example)---
-issp %>% 
-  group_by(c_alphan) %>%
-  filter(!is.na(class)) %>% 
-  summarise(n(),mean=mean(scale1,na.rm = TRUE), sd=sd(scale1,na.rm = TRUE))%>%
-  print(n = 40)
-
-##ANOVA Scale1 by country (EXAMPLE)-
-anova_country_scale1 <- aov(scale1~country, data = issp)
-summary(anova_country_scale2)
-
-
-
-# New scale 2 (para probar) -------------------------------------------------
-
-issp$scale2 <- ((issp$v42 + issp$v45 + issp$v46 + issp$v47)/16)*100
-frq(issp$scale2)
-summary(issp$scale2)
-mean(issp$scale2, na.rm = T)
-sd(issp$scale2, na.rm = T)
-
-## Scale 2 by class (example)-
-issp %>% 
-  group_by(class) %>%
-  filter(!is.na(class)) %>% 
-  summarise(n(),mean=mean(scale2,na.rm = TRUE),sd=sd(scale2,na.rm = TRUE))
-
-
-##ANOVA Scale2 by class (EXAMPLE---
-anova_class_scale2 <- aov(scale2~class, data = issp)
-summary(anova_class_scale2)
-
-
-## Scale 2 by country (example)---
-issp %>% 
-  group_by(c_alphan) %>%
-  filter(!is.na(class)) %>% 
-  summarise(n(),mean=mean(scale2,na.rm = TRUE),sd=sd(scale2,na.rm = TRUE))%>%
-  print(n = 40)
-
-
-##ANOVA Scale2 by country (EXAMPLE)-
-anova_country_scale2 <- aov(scale2~country, data = issp)
-summary(anova_country_scale2)
-
-
-
-#Two scales by class and country  --------------------------------------
-##Class
-issp %>% 
-  group_by(class) %>%
-  filter(!is.na(class)) %>% 
-  summarise(n(),mean1=mean(scale1,na.rm = TRUE),mean2=mean(scale2,na.rm = TRUE))
-
-##Country 
-issp %>% 
-  group_by(c_alphan) %>%
-  filter(!is.na(class)) %>% 
-  summarise(n(),mean1=mean(scale1,na.rm = TRUE),mean2=mean(scale2,na.rm = TRUE))%>%
-  print(n = 40)
-
+# Create scales -----------------------------------------------------------
+issp <- issp %>% 
+  select(2:7,starts_with("v4"), class, c_alphan, WEIGHT) %>%
+  filter_at(vars(v42:v47), all_vars(!is.na(.))) %>%
+  mutate_at(vars(starts_with("v4")), ~.*100/6) %>%
+  rowwise() %>% 
+  mutate(scale_1 = mean(v42:v47, na.rm =  T),
+         scale_2 = mean(c(v42,v45,v46,v47),  na.rm =  T)) %>%
+  ungroup() %>% 
+  mutate_at(vars(3:7, -AGE), ~as_factor(.)) %>% 
+  select(-starts_with("v4"), -scale_1)
 
 # 6. Save  ----------------------------------------------------------------------
 saveRDS(issp, file = "input/data/proc/issp-paper.rds")
