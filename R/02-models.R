61e49c2fa7fc2525f47ac1c40ea92ed18e3251e9
Pablo-Perez-Ahumada <po.perez.a@gmail.com>
2021-08-13 11:41:35 -0400

    cambios a graficos
    Graficos marginal effects

--- R/02-models.R
+++ R/02-models.R
@@ -26,8 +26,15 @@ model1_cse <-estimatr::lm_robust(scale_2 ~ AGE + SEX+ TYPORG2 + UNION + class_2 
 summary(model1_cse)
 parameters::model_parameters(model1_cse)
 
+#Model 1a = INTERACTION class*country (Warning: model matrix rank deficient)
+model1a_cse <-estimatr::lm_robust(scale_2 ~ AGE + SEX+ TYPORG2 + UNION + class_2 + 
+                                   c_alphan + class_2*c_alphan,
+                                 data = issp, weights = issp$WEIGHT,
+                                 clusters = c_alphan)
+summary(model1a_cse)
+parameters::model_parameters(model1a_cse)
 
-###  TableS----------
+###  TABLES----------
 
 stargazer(model1_cse,
           type="html",
@@ -64,19 +71,71 @@ apa.reg.table(model1_cse,filename="Table_model1_cse.doc") #Doesn't work
 
 
 # 4. Figures  ---------------------------------------------------------------
-#1- Predicted values for each classes, based on coef of model1_cse
+#1- Predicted values for each CLASS, based on coef of model1_cse
 ggeffects::ggpredict(model1_cse, terms = c("class_2")) %>% 
+  plot(., color = "greyscale",
+       ci.style = "errorbar")+
+  labs(title = "", x = "", y = "Perception of conflict") +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, size = 10),
+        axis.text.y = element_text(vjust = 0.5, size = 10))+
+   scale_y_continuous(breaks=c(5,10,15,20,25,30), limits = c(5,30))
+
+
+#2- Predicted values for each COUNTRY, based on coef of model1_cse
+# Weighted mean (descriptive)
+ggeffects::ggpredict(model1_cse, terms = c("c_alphan")) %>% 
+  plot(., color = "greyscale",
+       ci.style = "errorbar") +
+  coord_flip()  +
+  geom_hline(aes(yintercept = 13.4),  #13.4 = regression intercept
+             linetype = "dashed") +
+  labs(title = "", x = "", y = "Perception of conflict") +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, size = 10),
+        axis.text.y = element_text(vjust = 0.5, size = 10))+
+  scale_y_continuous(breaks=c(5,10,15,20,25,30), limits = c(5,30))
+
+
+#AYUDAAA 3. CLASS and COUNTRY: all COUNTRIES... AYUDA CON ESTO#####
+#Quiero: 
+#1) cambiar "shape" para cada categoria de clase (triangulo, cuadrado, etc.) (lo intenté con labs(shape="class_2") pero no funcionó
+#2) Ordenar los países de menor a mayor (o mayor a menor), segun nuntajepuntaje vble dependiente
+#1- Predicted values for each classes, based on coef of model1_cse
+ggeffects::ggpredict(model1_cse, terms = c("c_alphan","class_2")) %>% 
   plot(., color = "greyscale",
        ci.style = "errorbar") +
   labs(title = "", x = "", y = "Perception of conflict") +
-  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, size = 12),
-        axis.text.y = element_text(vjust = 0.5, size = 12))+
-   scale_y_continuous(breaks=c(5,10,15,20,25), limits = c(5,30))
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, size = 10),
+        axis.text.y = element_text(vjust = 0.5, size = 10),
+        legend.position = "bottom") +
+  scale_y_continuous(breaks=c(5,10,15,20,25,30,35), limits = c(5,35))
 
 
-#1.a :another option BARRAS
 
-###PENDIENTE
+
+#4. CLASS and COUNTRY: SELECTED COUNTRIES
+ggeffects::ggpredict(model1_cse, terms = c("c_alphan[CL, AU, US, MX, SE]","class_2")) %>% 
+  plot(., color = "greyscale",
+       ci.style = "errorbar") +
+  labs(title = "", x = "", y = "Perception of conflict") +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, size = 10),
+        axis.text.y = element_text(vjust = 0.5, size = 10))+
+  scale_y_continuous(breaks=c(5,10,15,20,25,30,35), limits = c(5,35))
+
+
+
+as.data.frame(ggeffects::ggpredict(model1_cse, terms = c("c_alphan"))) %>%
+  select(pred = predicted, everything()) %>%
+  geom_point() +
+  geom_errorbar(aes(ymin = conf.low, ymax = conf.high), stat = "identity") +
+  geom_hline(aes(yintercept = mean(pred_group)),
+             linetype = "dashed") +
+  facet_wrap(~ class_2, nrow = 2, dir = "h") +
+  guides(color = F) +
+  coord_flip()  +
+  scale_color_grey(start = 0.2, end = 0.5) +
+  labs(title = "", x = "Country code", y = "Perception of conflict")
+
+
 
 ### otros graficos para país y año
 
@@ -94,6 +153,11 @@ ggeffects::ggpredict(model1_cse, terms = c("c_alphan[CL, VE, US, MX]", "class_2"
   labs(title = "", x = "Country code (prefix ISO 3166)", y = "Conflict perception scale")
 
 
+
+
+
+
+
 # Manual graph ------------------------------------------------------------
 ## Colors
 issp <- issp %>% 
@@ -101,7 +165,7 @@ issp <- issp %>%
 
 color_group = setNames(as.character(issp$c_alphan), issp$color_group)
 
-# Figure 1 -------------------------------------------------------------------
+# Class & Country-------------------------------------------------------------------
 ## Definitiva
 as.data.frame(ggeffects::ggpredict(model1_cse, terms = c("c_alphan", "class_2"))) %>%
   select(class_2 = group, c_alphan = x, pred = predicted, everything()) %>%
@@ -118,5 +182,8 @@ as.data.frame(ggeffects::ggpredict(model1_cse, terms = c("c_alphan", "class_2"))
   coord_flip()  +
   scale_color_grey(start = 0.2, end = 0.5) +
   labs(title = "", x = "Country code", y = "Perception of conflict")
+
+
+##OTRA Opcion: selected countries
 
 
